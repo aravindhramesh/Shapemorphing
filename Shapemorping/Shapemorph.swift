d4f2b5e80af8d78a6a1c136bf1f7ab6ef267af3e
aravindhramesh <aravindh97@gmail.com>
2024-10-13 13:18:15 -0700

    Drag gesture to move the shapes around

--- Shapemorping/Shapemorph.swift
+++ Shapemorping/Shapemorph.swift
@@ -9,11 +9,30 @@ import SwiftUI
 
 struct morph: View {
     @State private var isAnimate = false
+    @State var offset: CGSize = .zero
     var body: some View {
        
         RoundedRectangle(cornerRadius: isAnimate ? 25.0 : 150.0)
             .fill(isAnimate ? .blue : .orange)
             .frame(width: 300,height: 300)
+            .offset(offset)
+            .gesture(
+                DragGesture()
+                    .onChanged {
+                        value in
+                        withAnimation(.spring()){
+                            offset = value.translation
+                    }
+                    }
+                    .onEnded(){
+                        value in
+                        withAnimation(.spring()){
+                            offset = value.translation
+                    }
+                        
+                    }
+            
+            )
             .onTapGesture {
                 withAnimation(.smooth(duration: 0.5)) {
                     isAnimate.toggle()
