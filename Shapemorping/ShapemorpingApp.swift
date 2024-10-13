a5ee5dc5ba64ebd2a312cdfa60ff2a947342ab37
aravindhramesh <aravindh97@gmail.com>
2024-10-12 23:50:29 -0700

    Initial Commit

---
+++ Shapemorping/ShapemorpingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapemorpingApp.swift
+//  Shapemorping
+//
+//  Created by Aravindh Ramesh on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShapemorpingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
