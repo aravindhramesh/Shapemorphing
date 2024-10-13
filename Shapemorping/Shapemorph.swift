a3a45ebe6c85b704cba89a886ae5e530069d99c9
aravindhramesh <aravindh97@gmail.com>
2024-10-13 01:04:26 -0700

    Development Smoother animation

--- Shapemorping/Shapemorph.swift
+++ Shapemorping/Shapemorph.swift
@@ -5,4 +5,21 @@
 //  Created by Aravindh Ramesh on 13/10/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct morph: View {
+    @State private var isAnimate = true
+    var body: some View {
+       
+        RoundedRectangle(cornerRadius: isAnimate ? 25.0 : 5000.0)
+            .frame(width: 300,height: 300)
+            .onTapGesture {
+                withAnimation(.smooth(duration: 0.5)) {
+                    isAnimate.toggle()
+                               }
+            }
+        
+    }
+}
+
+
